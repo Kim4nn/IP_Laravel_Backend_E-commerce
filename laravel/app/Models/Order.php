9858681331d91b45b9f778136ac248edcd4adf4c
Kimann <lykimann1@gmail.com>
2025-03-19 00:18:10 +0700

    Define mutator and accessor on Order and Payment model.

--- laravel/app/Models/Order.php
+++ laravel/app/Models/Order.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\Attribute;
+use Carbon\Carbon;
 
 class Order extends Model
 {
+    protected $table = 'orders';
     protected $fillable = ['order_date', 'total_price'];
 
     public function payments()
@@ -21,5 +24,16 @@ class Order extends Model
     public function order_products()
     {
         return $this->hasMany(OrderProduct::class);
+    }
+
+    protected function order_date(): Attribute
+    {
+        return Attribute::Make(
+            // Mutator: Convert input format to MySQL format before saving
+            set: fn ($value) => Carbon::createFromFormat('d/m/Y H:i:s', $value)->format('Y-m-d H:i:s'),
+
+            // Accessor: Convert database format to user format when retrieving
+            get: fn ($value) => Carbon::parse($value)->format('d/m/Y H:i:s')
+        );
     }
 }
--- laravel/app/Models/Payment.php
+++ laravel/app/Models/Payment.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\Attribute;
+use Carbon\Carbon;
 
 class Payment extends Model
 {
-    protected $fillable = ['payment_method', 'amount'];
+    protected $table = 'payments';
+    protected $fillable = ['payment_date', 'payment_method', 'amount'];
 
     public function customer()
     {
@@ -16,5 +19,16 @@ class Payment extends Model
     public function order()
     {
         return $this->belongsTo(Order::class);
+    }
+
+    protected function payment_date(): Attribute
+    {
+        return Atribute::make(
+            // Mutator: Convert input format to MySQL format before saving
+            set: fn ($value) => Carbon::createFromFormat('d/m/Y H:i:s', $value)->format('Y-m-d H:i:s'),
+
+            // Accessor: Convert database format to user format when retrieving
+            get: fn ($value) => Carbon::parse($value)->format('d/m/Y H:i:s')
+        )
     }
 }
