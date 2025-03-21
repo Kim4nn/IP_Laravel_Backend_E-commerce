cc7c47d52270ea208c23c58be1c9e0c20dadec0b
Kimann <lykimann1@gmail.com>
2025-03-21 14:29:53 +0700

    Create 8 new testcases and insert comments.

---
+++ laravel/tests/Feature/CartTest.php
@@ -0,0 +1,17 @@
+<?php
+
+Class CartTest extends TestCase
+{
+    /**
+     * Test ID: Cart-001
+     * Description: Check if we can access the get all carts api
+     * Precondition: None
+     * Test steps: 1. Hit the get all carts api
+     *             2. Check if the response status is 200
+     * Test Data: None
+     * Expected result: The response status should be 200
+     * Actual Result: The response status is 200
+     * Status: Passed
+     * Remark: None
+     */
+}
