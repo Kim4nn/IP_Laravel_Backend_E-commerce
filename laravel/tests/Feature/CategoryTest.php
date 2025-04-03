eb76c1c0a8edeb072809f7c457d494b14e21041b
Kimann <lykimann1@gmail.com>
2025-04-03 14:44:45 +0700

    1. Convert test files format from Pest to PHPUnit. 2. Delete Pest dependency and install PHPUnit. 3. Add Dockerfile to install composer.

--- laravel/tests/Feature/CategoryTest.php
+++ laravel/tests/Feature/CategoryTest.php
@@ -1,12 +1,16 @@
 <?php
 
-Class CategoryTest extends TestCase
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
 {
     /**
      * Test ID: Category-001
-     * Description: Check if we can access the get all carts api
+     * Description: Check if we can access the get all categories api
      * Precondition: None
-     * Test steps: 1. Hit the get all carts api
+     * Test steps: 1. Hit the get all cateogories api
      *             2. Check if the response status is 200
      * Test Data: None
      * Expected result: The response status should be 200
@@ -14,4 +18,12 @@ Class CategoryTest extends TestCase
      * Status: Passed
      * Remark: None
      */
+
+    public function check_if_we_can_access_the_get_all_categories_api(): void
+    {
+        $response = $this->get('/api/categories');
+
+        // Assert that the response has a successful (>=200 and <300) HTTP status code
+        $response->assertSuccessful();
+    }
 }
--- laravel/tests/Feature/ProductTest.php
+++ laravel/tests/Feature/ProductTest.php
@@ -1,12 +1,16 @@
 <?php
 
-Class ProductTest extends TestCase
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ProductTest extends TestCase
 {
     /**
      * Test ID: Product-001
-     * Description: Check if we can access the get all carts api
+     * Description: Check if we can access the get all products api
      * Precondition: None
-     * Test steps: 1. Hit the get all carts api
+     * Test steps: 1. Hit the get all products api
      *             2. Check if the response status is 200
      * Test Data: None
      * Expected result: The response status should be 200
@@ -14,4 +18,12 @@ Class ProductTest extends TestCase
      * Status: Passed
      * Remark: None
      */
+
+    //
+    public function check_if_we_can_access_the_get_all_products_api(): void
+    {
+        $response = $this->get('/api/products');
+
+        $response->assertStatus(200);
+    }
 }
