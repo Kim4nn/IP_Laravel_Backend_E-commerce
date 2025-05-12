e650a189116eb420290f3b03c8fd96d7ca32a215
Kimann <lykimann1@gmail.com>
2025-05-12 15:44:08 +0700

    Verify, setup, and test uploading files.

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,6 +11,11 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/upload', function () {
+    return view('upload_file');
+});
+Route::post('/upload', [UploadController::class, 'upload'])->name('upload');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
